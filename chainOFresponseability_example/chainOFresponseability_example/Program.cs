6e3b63c330c5f620a30fcf75e269527901587ba9
reza <rezamahmoudinik1380@gmail.com>
2022-01-05 21:53:32 +0330

    commiting from pc

---
+++ chainOFresponseability_example/chainOFresponseability_example/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace chainOFresponseability_example
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            handler num1 = new Concretehanler_one();
+            handler num2 = new Concretehanler_tow();
+            handler num3 = new Concretehanler_three();
+            num1.setsuccessor(num2);
+            num2.setsuccessor(num3);
+            int[] req = { 2, 5, 4, 15, 8, 12, 24, 25 };
+            foreach (int rqs in req)
+            {
+                num1.handlerRequest(rqs);
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ chainOFresponseability_example/chainOFresponseability_example/handler.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace chainOFresponseability_example
+{
+    abstract class handler
+    {
+        protected handler successor;
+        public void setsuccessor(handler successor)
+        {
+            this.successor = successor;
+        }
+        public abstract void handlerRequest(int rqs);
+        internal abstract void setsuccessor(int rqs);
+    }
+
+    //Does not recognize subclasses
+  internal class Concretehanler_one : handler
+    {
+
+        public override void handlerRequest(int rqs)
+        {
+            if (rqs >= 0 && rqs < 10)
+            {
+                Console.WriteLine("[0] request by [1]", this.GetType().Name, rqs);
+            }
+            else if (successor != null)
+            {
+                successor.handlerRequest(rqs);
+            }
+        }
+
+        internal override void setsuccessor(int rqs)
+        {
+            if (rqs >= 1 && rqs < 10)
+            {
+                Console.WriteLine("[0] request by [1]", this.GetType().Name, rqs);
+            }
+            else if (successor != null)
+            {
+                successor.handlerRequest(rqs);
+            }
+        }
+    }
+
+   internal  class Concretehanler_tow : handler
+    {
+        public override void handlerRequest(int rqs)
+        {
+            if (rqs >= 5 && rqs < 10)
+            {
+                Console.WriteLine("[0] request by [1]", this.GetType().Name, rqs);
+            }
+            else if (successor != null)
+            {
+                successor.handlerRequest(rqs);
+            }
+        }
+
+        internal override void setsuccessor(int rqs)
+        {
+            if (rqs >= 20 && rqs < 10)
+            {
+                Console.WriteLine("[0] request by [1]", this.GetType().Name, rqs);
+            }
+            else if (successor != null)
+            {
+                successor.handlerRequest(rqs);
+            }
+        }
+    }
+
+    internal class Concretehanler_three : handler
+    {
+        public override void handlerRequest(int rqs)
+        {
+            if (rqs >= 20 && rqs < 10)
+            {
+                Console.WriteLine("[0] request by [1]", this.GetType().Name, rqs);
+            }
+            else if (successor != null)
+            {
+                successor.handlerRequest(rqs);
+            }
+        }
+
+        internal override void setsuccessor(int rqs)
+        {
+            if (rqs >= 20 && rqs < 10)
+            {
+                Console.WriteLine("[0] request by [1]", this.GetType().Name, rqs);
+            }
+            else if (successor != null)
+            {
+                successor.handlerRequest(rqs);
+            }
+        }
+    }
+}
